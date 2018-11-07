05903c44d23c6cd3aa5926249546a8ecd49513b0
Xavi <Xavi@DESKTOP-V4F3JI2>
2018-11-07 19:42:17 +0100

    mongo db

---
+++ README.md
@@ -0,0 +1,2 @@
+- Configurar mongoDB con xampp
+https://www.roytuts.com/mongodb-php7-xampp-windows/
--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new FOS\ElasticaBundle\FOSElasticaBundle(),
+            new Doctrine\Bundle\MongoDBBundle\DoctrineMongoDBBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
